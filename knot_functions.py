891adcd855db9d74a180f7edc88a853c0d81498f
Justin Lee <justin.adam.lee@gmail.com>
2024-06-17 11:29:33 -0400

    testing knot funcs

---
+++ knot_functions.py
@@ -0,0 +1,28 @@
+from typing import Callable
+from matplotlib import pyplot as plt
+
+
+def knot_function(i: int, k: int, knots: list[float]) -> Callable:
+    if k == 0:
+        return lambda t: int(knots[i] <= t < knots[i + 1])
+
+    return lambda t: \
+        (t - knots[i]) / (knots[i + k] - knots[i]) \
+        * knot_function(i, k - 1, knots)(t) \
+        + (knots[i + k + 1] - t) / (knots[i + k + 1] - knots[i + 1]) \
+        * knot_function(i + 1, k - 1, knots)(t)  # fmt: skip
+
+
+t = 3.23
+
+knot_vector = [*range(7)]
+k = 2
+c = [5, -5, 3, 2]
+
+
+bases = [knot_function(n, k, knot_vector) for n in range(len(knot_vector) - k - 1)]
+x = [i / 20 for i in range(120)]
+y = [sum(i * j(t) for i, j in zip(c, bases)) for t in x]
+
+plt.scatter(x, y)
+plt.show()
